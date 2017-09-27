c39f65772d3ec0851c0d3f6ab7c651fd8c78e7a6
Iron Mastiff <1760298283@qq.com>
2017-09-28 00:34:13 +0800

    Add new function

--- MNIST.py
+++ MNIST.py
@@ -1,7 +1,10 @@
-from Util import  fit_data, model
+from Util import  fit_data, model, predict, print_iamge
 
 train_X, train_Y, test_X, test_Y = fit_data()
-model( train_X, train_Y, test_X, test_Y )
+parameters = model( train_X, train_Y, test_X, test_Y )
+predict( parameters, test_X, 6 )
+print_iamge(test_X, test_Y, 6 )
+
 
 
 
